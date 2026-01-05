2e40c97c113f10371d5254f5608db4b7f3daf9f6
Akhilesh Shahade <shahadeakhilesh@gmail.com>
2026-01-05 11:06:03 +0000

    Added searchfield

--- capm_firstProj/app/empmgmtui/webapp/controller/View1.controller.js
+++ capm_firstProj/app/empmgmtui/webapp/controller/View1.controller.js
@@ -1,10 +1,19 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter"
+], (Controller, Filter) => {
     "use strict";
 
     return Controller.extend("com.demo.empmgmtui.controller.View1", {
         onInit() {
+        },
+        onSearch:function(oEvent){
+            var aFilters = [];
+            var name = oEvent.getSource().getValue();
+            if(name){
+                aFilters.push(new Filter("name", "Contains", name));
+            }
+            this.byId("EmpTab").getBinding("items").filter(aFilters);
         }
     });
 });
