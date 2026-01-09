c4b6ed7238e086cb010b118d2d1f28625d467016
Akhilesh Shahade <shahadeakhilesh@gmail.com>
2026-01-09 11:17:25 +0000

    Deployd DB and Create Operation

--- capm_firstProj/app/empmgmtui/webapp/controller/View1.controller.js
+++ capm_firstProj/app/empmgmtui/webapp/controller/View1.controller.js
@@ -1,25 +1,50 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
-], (Controller, Filter) => {
+    "sap/m/MessageBox"
+], (Controller, Filter, MessageBox) => {
     "use strict";
 
     return Controller.extend("com.demo.empmgmtui.controller.View1", {
         onInit() {
         },
-        onSearch:function(oEvent){
+        onSearch: function (oEvent) {
             var aFilters = [];
             var name = oEvent.getSource().getValue();
-            if(name){
+            if (name) {
                 aFilters.push(new Filter("name", "Contains", name));
             }
             this.byId("EmpTab").getBinding("items").filter(aFilters);
         },
-        onPressRow:function(oEvent) {
+        onPressRow: function (oEvent) {
             var sEmpId = oEvent.getSource().getBindingContext().getObject().ID;
-            this.getOwnerComponent().getRouter().navTo("RouteView2",{
-                EMPID:sEmpId
+            this.getOwnerComponent().getRouter().navTo("RouteView2", {
+                EMPID: sEmpId
             });
+        },
+        onCreateEmpOpen: function () {
+            var tableBinding = this.byId("EmpTab").getBinding("items");
+            var oBindingContext = tableBinding.create(); //give a new binding context (memory)
+            this.oBindingContext = oBindingContext;
+            this.oBindingContext.created().then(function () {
+                MessageBox.success(this.oBindingContext.getObject().ID);
+                this._createEmpFrag.close();
+            }.bind(this), function (error) {
+                MessageBox.error(error);
+            });
+            if (!this._createEmpFrag) {
+                this._createEmpFrag = sap.ui.xmlfragment(this.getView().getId(), "com.demo.empmgmtui.view.fragments.CreateNewEmp", this);
+                this.getView().addDependent(this._createEmpFrag);
+            }
+            this._createEmpFrag.setBindingContext(oBindingContext);
+            this._createEmpFrag.open();
+        },
+        onPressSave: function () {
+            var oModel = this.getOwnerComponent().getModel();
+            oModel.submitBatch("EmpGrp"); //UpdateGroupId from manifest.json file
+        },
+        onPressCancel: function () {
+            this._createEmpFrag.close();
         }
     });
 });
